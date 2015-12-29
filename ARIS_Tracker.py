cd01d8c96ff219f745e7690396d6973404277de1
Chris Rillahan <chris.rillahan@gmail.com>
2015-12-29 14:21:36 -0500

    fully functional playback.  Backed up file as well.

--- ARIS_Tracker.py
+++ ARIS_Tracker.py
@@ -1,18 +1,24 @@
-# Chris Rillahan
-#ARIS Tracker
+#ARIS_Tracker.py
+#Created by Chris Rillahan
+#Last Updated: 12/29/2015
+#Written with Python 2.7.11, OpenCV 3.0
+
+#This script simply plays back an ARIS file at present. The ARIS data has been
+#exported as a *.mp4 file.
 
 import cv2
 
+#Set the path and file location
 path = "C:\\Users\\User\\OneDrive\\ARIS\\ARIS_Tracker\\2015-06-03_023000.mp4"
 print(path)
 
+#Load the video
 video = cv2.VideoCapture(path)
-cv2.namedWindow('Video', flags = cv2.WINDOW_NORMAL)
 #print(video.isOpened())
 
+#Create the window and start the loop.
+cv2.namedWindow('Video', flags = cv2.WINDOW_NORMAL)
 success = True
-
-
 while success == True:
     success, image = video.read()
     FPS = video.get(cv2.CAP_PROP_FPS)
@@ -20,8 +26,7 @@ while success == True:
 #    cv2.waitKey(int(FPS/60*1000)) #Actual Playback Speed
     k = cv2.waitKey(10)
     if k == 27:
-            break
             video.release()
-            cv2.destroyAllWindows()
+            break
     
 cv2.destroyAllWindows()
