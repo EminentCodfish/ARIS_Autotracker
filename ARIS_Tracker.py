0c2ec2e0aa9ac27e3d845d6ec73affa621e3e687
Chris Rillahan <chris.rillahan@gmail.com>
2016-01-07 14:00:56 -0500

    Implemented the object detection section

--- ARIS_Tracker.py
+++ ARIS_Tracker.py
@@ -19,53 +19,61 @@ print(path)
 #Load the video
 video = cv2.VideoCapture(path)
 #print(video.isOpened())
+
+#Read the first frame to serve as a background subtraction frame
 _, avg = video.read()
 avg1 = cv2.cvtColor(avg, cv2.COLOR_BGR2GRAY)
 avg2 = np.float32(avg1)
 
-
+#3x3 kernal for the erosion and dilation procedures
 kernel = np.ones((3,3), np.uint8)
 
 #Create the window and start the loop.
 cv2.namedWindow('Video', flags = cv2.WINDOW_NORMAL)
-#cv2.namedWindow('KNN', flags = cv2.WINDOW_NORMAL)
-cv2.namedWindow('Ave', flags = cv2.WINDOW_NORMAL)
+cv2.namedWindow('Threshold', flags = cv2.WINDOW_NORMAL)
 
 success = True
 while success == True:
+    #Load video and convert to grayscale
     success, image = video.read()
     bwimage = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
     FPS = video.get(cv2.CAP_PROP_FPS)
     
+    #Create the running average background mask and subtract from the foreground
     cv2.accumulateWeighted(bwimage, avg2, 0.01)
     res1 = cv2.convertScaleAbs(avg1)
     fg = cv2.subtract(bwimage, res1)
     
-    #fg2 = cv2.medianBlur(fg, 5)
+    #Clean up the image (amplify, threshold, open and close).
     fg3 = cv2.add(fg, fg)
     _,thr = cv2.threshold(fg3, 20, 254, cv2.THRESH_BINARY)
     morphEx = cv2.morphologyEx(thr, cv2.MORPH_OPEN, kernel)
     morphEx2 = cv2.morphologyEx(morphEx, cv2.MORPH_CLOSE, kernel)
 
+    #Find the contours and filter based on the area
     cimg,contours0, hier = cv2.findContours(morphEx2, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    print 'new'
-    print contours0
-    contours = [cv2.approxPolyDP(cnt, 3, True) for cnt in contours0]
     contours1 = [cv2.contourArea(cnt) for cnt in contours0]
-    #print contours
+    cont_filt = [contours0[i] for i in range(len(contours1)) if contours1[i] > 30.0]
     print contours1
-    #filt_cont = 
-    cv2.drawContours(image, contours0, -1, (128,255,255), 3)
-
-    #run through contour areas to filter out small contours.
     
+    #Draw the contours and rotated boundary boxes.
+    cv2.drawContours(image, cont_filt, -1, (128,255,255), 3)
+    
+    for i in range(len(cont_filt)):
+        rect = cv2.minAreaRect(cont_filt[i])
+        box = cv2.boxPoints(rect)
+        box = np.int0(box)
+        cv2.drawContours(image, [box], 0, (0,0,255), 2)
+    
+    #Display
     cv2.imshow('Video', image)
-    #cv2.imshow('KNN', thr)
-    cv2.imshow('Ave', morphEx2)
+    cv2.imshow('Threshold', morphEx2)
 #    cv2.waitKey(int(FPS/60*1000)) #Actual Playback Speed
     k = cv2.waitKey(10)
     if k == 27:
             video.release()
             break
     
+#Clean up
 cv2.destroyAllWindows()
+video.release()
